6cbfd3612aa5d67b0cfe6de85623d1e9847bf049
eugeniadayoub <eugeniav.dayoub@javeriana.edu.co>
2025-04-22 15:41:15 -0500

    Actualizacion

--- demo/src/main/resources/static/js/scriptProgramaCitaPresencial.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector(".formulario-cita");
-    
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert("¡Tu cita ha sido agendada! Pronto nos pondremos en contacto contigo.");
-        form.reset();
-    });
-});
